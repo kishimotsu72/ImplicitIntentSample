ff4b0a92c76465f04c11c1b42ab2177f6fa2d02b
kishimotsu72 <kishimotsu72@gmail.com>
2018-07-11 11:48:02 +0900

    地図検索ボタンタップ時の処理

--- app/src/main/java/com/websarva/wings/android/implicitintentsample/IntentStartActivity.java
+++ app/src/main/java/com/websarva/wings/android/implicitintentsample/IntentStartActivity.java
@@ -1,7 +1,22 @@
 package com.websarva.wings.android.implicitintentsample;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class IntentStartActivity extends AppCompatActivity {
 
@@ -9,5 +24,20 @@ public class IntentStartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_start);
+    }
+    public void onMapSearchButtonClick(View view) {
+        EditText etSearchWord = findViewById(R.id.etSearchWord);
+        String searchWord = etSearchWord.getText().toString();
+
+        try {
+            searchWord = URLEncoder.encode(searchWord, "UTF-8");
+            String uriStr = "geo:0,0?q=" + searchWord;
+            Uri uri = Uri.parse(uriStr);
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+        catch(UnsupportedEncodingException ex) {
+            Log.e("IntentStartActivity", "検索キーワード変換失敗", ex);
+        }
     }
 }
